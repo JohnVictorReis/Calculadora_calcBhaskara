74b53c6d6616c841c6ee4f1ad116b6f6d8e02441
JohnVictorReis <johnvictor.trabalho@gmail.com>
2022-04-28 09:48:55 -0300

    Criando o projeto de Bhaskara

--- Bhaskara/bhaskara.js
+++ Bhaskara/bhaskara.js
@@ -1,27 +1,59 @@
-function calcDelta(a,b,c){
+function calcDelta(a, b, c) {
 
-    var delta = Math.pow(b,2) - (4*a*c)
+  var delta = Math.pow(b,2) - (4 * a * c);
+  var resultbhaskara = calcBhaskara(a, b, delta)
+
+ return delta;
+
+}
+
+function calcBhaskara(a, b, delta) {
+
+  var x1 = 0;
+  var x2 = 0;
+
+  if (delta > 0) {
+    x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+    if(x1 <=0 )
+    x1 = "Valor inexistente";
+
+    if(x2 <= 0)
+    x2 = "Valor inexistente";
+  } else if (delta == 0) {
+    x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    x2 = "indefinido";
+  } else {
     
-    return delta
-}
+    x1 = "Valor inexistente";
+    x2 = "Valor inexistente";
+  }
 
-function calcX(a,b,delta){
-
-    if(delta < 0 ){
-        print('Não calculamos valores de delta negativo')
-    }else{
-        if(delta > 0){
-
-            var x1 = (-b + Math.sqrt(delta)) / 2*a
-            var x2 = (-b - Math.sqrt(delta)) / 2*a
-
-            return x1,x2
-        }
-    }
+  return  {x1, x2};
 }
 
 
+function enviar(){
 
-export {calcDelta, calcX}
+    let a = document.getElementById("aaa").value;
+    let b = document.getElementById("bbb").value;
+    let c = document.getElementById("ccc").value;
+
+    var delta = calcDelta(a,b,c);
+    var resultbhaskara = calcBhaskara(a, b, delta)
+
+    document.getElementById("resultado").innerHTML="   " +  "x' = " + resultbhaskara.x1 + " x'' = " + resultbhaskara.x2 + "   "
+
+    
+}
 
 
+//const abc = document.getElementById("getElementById("fomulario")");
+//document.getElementById("blau").innerHTML = x1;
+//document.getElementById("botao").addEventListener("click", calcDelta(0, 0, 0));
+
+document.getElementById("botao").addEventListener("click", enviar);
+
+
+export { calcDelta, calcBhaskara, enviar };
--- Bhaskara/calcBhaskara.test.js
+++ Bhaskara/calcBhaskara.test.js
@@ -1,12 +1,32 @@
-import { calcDelta } from "./bhaskara"
+import { calcDelta } from "./bhaskara";
+import { calcBhaskara } from "./bhaskara";
+import { enviar } from "./bhaskara";
 
 //Teste de calculo delta
-it('Descobrindo o delta com três valores (B* - 4xAxC)',() => {
-    expect(calcDelta(2,8,-24)).toBe(256)
-})
+it("Descobrindo o delta com três valores (B* - 4xAxC)", () => {
+  expect(calcDelta(2, 8, -24)).toBe(256);
+});
 
-//Teste de calculo x1 e x2
+///Teste de calculo delta > 0
+it("Calculando o valor de Bhaskara com Delta-Δ sendo maior que zero", () => {
+    expect(calcBhaskara(1, -5, calcDelta(1, -5, 6))).toMatchObject({
+      x1: 3,
+      x2: 2,
+    });
+  });
 
-it('Descobrindo o valor de x1 (-B + Raiz de Delta / 2.A)' , () => {
-    expect(calcX(1,12,delta)).toBe(1)
-})
+//Teste de calculo delta == 0
+it("Calculando o valor de Bhaskara com Delta-Δ sendo igual a zero", () => {
+  expect(calcBhaskara(4, 4, calcDelta(4, 4, 1))).toMatchObject({
+    x1: -0.5,
+    x2: "indefinido",
+  });
+});
+
+//Teste de calculo delta < 0
+it("Calculando o valor de Bhaskara com Delta-Δ sendo menor que zero", () => {
+  expect(calcBhaskara(4, 5, calcDelta(4, 5, 0))).toMatchObject({
+    x1: "Valor inexistente",
+    x2: "Valor inexistente",
+  });
+});
